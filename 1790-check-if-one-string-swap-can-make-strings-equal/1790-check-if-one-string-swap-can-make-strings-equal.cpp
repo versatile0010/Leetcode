a4af22aa548e0eb7f4d84e2dd88d6138cc1462b1
JaeHyeon Lee <wogusn@gmail.com>
2022-10-13 16:20:20 +0900

    Time: 123 ms (5.02%), Space: 31 MB (5.35%) - LeetHub

--- 1790-check-if-one-string-swap-can-make-strings-equal/1790-check-if-one-string-swap-can-make-strings-equal.cpp
+++ 1790-check-if-one-string-swap-can-make-strings-equal/1790-check-if-one-string-swap-can-make-strings-equal.cpp
@@ -1,19 +1,26 @@
-// O(N)
 class Solution {
 public:
+    string swap_string(string &s, int x, int y) {
+        char temp_char; string temp = s;
+        temp_char = temp[x];
+        temp[x] = temp[y];
+        temp[y] = temp_char;
+        return temp;
+ }
+
     bool areAlmostEqual(string s1, string s2) {
-        // check frequency of characters
-        vector<int> arr(26); int cnt = 0; // # of mismatched positions
-        for (int i = 0; i < s1.length(); i++) {
-            arr[s1[i] - 'a']++;
-            arr[s2[i] - 'a']--;
+        if (s1 == s2) return true;
+        string temp = ""; bool ans = false;
+        for (int i = 0; i < s1.length()-1; i++) {
+            temp.clear();
+            for (int j = i+1; j < s1.length(); j++) {
+                temp = swap_string(s2, i, j);
+                if (temp == s1) {
+                    ans = true;
+                    break;
+                }
+            }
         }
-        for (int i = 0; i < arr.size(); i++) {
-            if (arr[i] != 0) return false;
-        }
-        for (int i = 0; i < s1.length(); i++) {
-            if (s1[i] != s2[i]) cnt++;
-        }
-        return cnt == 0 || cnt == 2 ? true : false;
+        return ans;
     }
 };
